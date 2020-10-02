290ec8b0f3c20a3f4d029a96ea7e96d946ba0600
Ivan Kopcik <ivan.kopcik@gmail.com>
2020-10-02 16:51:58 +0200

    :art: GENERAL - added link to static page into cookie consent

--- v1/gdpr/cookie-consent.js
+++ v1/gdpr/cookie-consent.js
@@ -1,21 +1,33 @@
 import withSession from "grandus-lib/utils/session";
+import { reqGetHost } from "grandus-lib/utils";
+import { get } from "lodash";
 import { LEGAL_COOKIE_CONSENT } from "grandus-lib/constants/SessionConstants";
+
+const generateOutput = (webinstance, consent) => {
+  return {
+    accepted: consent ? true : false,
+    link: get(webinstance, "globalSettings.cookie_policy_page", null),
+  };
+};
 
 export default withSession(async (req, res) => {
   const { method } = req;
 
+  const webinstance = await fetch(
+    `${reqGetHost(req)}/api/lib/v1/webinstance`
+  ).then((result) => result.json());
+
   switch (method) {
     case "GET":
       const cookieConsent = req.session.get(LEGAL_COOKIE_CONSENT);
-
-      res.status(200).json({ accepted: cookieConsent ? true : false });
+      res.status(200).json(generateOutput(webinstance, cookieConsent));
       break;
 
     case "PUT":
       req.session.set(LEGAL_COOKIE_CONSENT, true);
       await req.session.save();
 
-      res.status(200).json({ accepted: true });
+      res.status(200).json(generateOutput(webinstance, cookieConsent));
       break;
 
     default:
