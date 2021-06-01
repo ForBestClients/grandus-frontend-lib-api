be658cb9305a898e965d5e683cd1023d5f8d7aa7
Pavol Rasev <pavol@forbestclients.com>
2021-06-01 13:52:48 +0200

    :sparkles: GENERAL - added user checkt email endpoint

---
+++ v1/user/check-email.js
@@ -0,0 +1,14 @@
+import { reqGetHeaders, reqApiHost } from "grandus-lib/utils";
+
+export default async (req, res) => {
+  const { body } = req;
+  const jsonBody = JSON.parse(body);
+
+  const response = await fetch(`${reqApiHost(req)}/api/v2/users/check-email`, {
+    method: 'POST',
+    headers: reqGetHeaders(req),
+    body: JSON.stringify({ user: { email: jsonBody?.email } }),
+  }).then((result) => result.json());
+
+  res.status(200).json(response?.data);
+};
