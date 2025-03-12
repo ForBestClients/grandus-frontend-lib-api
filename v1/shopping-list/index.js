a32f22abf0fb6b6b44254f35c1dd9b0e3aa85690
András Risnyovszký <andras@forbestclients.com>
2025-03-12 08:19:52 +0100

    SHOPPING LIST - separate listing fields

--- v1/shopping-list/index.js
+++ v1/shopping-list/index.js
@@ -1,46 +1,54 @@
-import withSession from "grandus-lib/utils/session";
-import { reqGetHeaders, reqApiHost, getApiExpand } from "grandus-lib/utils";;
-import get from "lodash/get";
+import withSession from 'grandus-lib/utils/session';
+import { reqGetHeaders, reqApiHost, getApiExpand } from 'grandus-lib/utils';
+import get from 'lodash/get';
 
 export default withSession(async (req, res) => {
   const { body = {}, method } = req;
 
-  res.setHeader("Content-Type", "application/json");
+  res.setHeader('Content-Type', 'application/json');
 
   let shoppingList = null;
   let url = `${reqApiHost(req)}/api/v2/shopping-lists`;
 
-  if (getApiExpand("SHOPPING_LIST")) {
-    url += "?" + getApiExpand("SHOPPING_LIST", true);
+  if (getApiExpand('SHOPPING_LIST')) {
+    url += '?' + getApiExpand('SHOPPING_LIST', true);
   }
 
-  if (getApiExpand("SHOPPING_LIST", false, "FIELDS")) {
+  if (getApiExpand('SHOPPING_LIST_LISTING', false, 'FIELDS')) {
     url +=
-      (getApiExpand("SHOPPING_LIST") ? "&" : "?") +
-      getApiExpand("SHOPPING_LIST", true, "FIELDS");
+      (getApiExpand('SHOPPING_LIST_LISTING') ? '&' : '?') +
+      getApiExpand('SHOPPING_LIST_LISTING', true, 'FIELDS');
+  } else if (getApiExpand('SHOPPING_LIST', false, 'FIELDS')) {
+    url +=
+      (getApiExpand('SHOPPING_LIST') ? '&' : '?') +
+      getApiExpand('SHOPPING_LIST', true, 'FIELDS');
   }
 
   switch (method) {
-    case "GET":
+    case 'GET':
       shoppingList = await fetch(url, {
         headers: reqGetHeaders(req),
-      }).then(async (result) => await result.json());
+      }).then(async result => await result.json());
 
-      res.status(get(shoppingList, "statusCode", 500)).json(get(shoppingList, "data"));
+      res
+        .status(get(shoppingList, 'statusCode', 500))
+        .json(get(shoppingList, 'data'));
       break;
 
-    case "POST":
+    case 'POST':
       shoppingList = await fetch(url, {
         headers: reqGetHeaders(req),
-        method: "POST",
+        method: 'POST',
         body: body,
-      }).then((result) => result.json());
+      }).then(result => result.json());
 
-      res.status(get(shoppingList, "statusCode", 500)).json(get(shoppingList, "data"));
+      res
+        .status(get(shoppingList, 'statusCode', 500))
+        .json(get(shoppingList, 'data'));
       break;
 
     default:
-      res.setHeader("Allow", ["GET", "POST"]);
+      res.setHeader('Allow', ['GET', 'POST']);
       res.status(405).end(`Method ${method} Not Allowed`);
   }
 });
