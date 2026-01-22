83c0897fcc4e1729331c0536092df121a6ae9a82
Ivan Kopčík <ivan.kopcik@gmail.com>
2026-01-22 23:51:32 +0100

    antispam

--- v1/cart/contact/index.js
+++ v1/cart/contact/index.js
@@ -1,5 +1,8 @@
 import withSession from "grandus-lib/utils/session";
 import { CART_CONTACT_CONSTANT } from "grandus-lib/constants/SessionConstants";
+import { getClientIpAddress } from "grandus-lib/utils/request";
+import { validateHoneypot, checkRateLimit } from "utils/antiSpam";
+import client from "grandus-lib/utils/cache";
 import { isEmpty } from 'lodash';
 
 export default withSession(async (req, res) => {
@@ -19,7 +22,37 @@ export default withSession(async (req, res) => {
       break;
 
     case "POST":
-      req.session.set(CART_CONTACT_CONSTANT, body);
+      // Anti-spam checks
+      const ip = getClientIpAddress(req);
+
+      // 1. Honeypot validation
+      if (validateHoneypot(body._website)) {
+        res.statusCode = 400;
+        return res.json({
+          error: 'Neplatný request',
+          status: false
+        });
+      }
+
+      // 2. Rate limiting (max 5 za hodinu - vyšší limit pre košík)
+      const isRateLimited = await checkRateLimit(ip, client, {
+        keyPrefix: 'contact_form_cart',
+        maxRequests: 5,
+        windowSeconds: 3600
+      });
+
+      if (isRateLimited) {
+        res.statusCode = 429;
+        return res.json({
+          error: 'Príliš veľa požiadaviek. Skúste neskôr.',
+          status: false
+        });
+      }
+
+      // Odstrániť honeypot field pred uložením do session
+      const { _website, ...cleanBody } = body;
+
+      req.session.set(CART_CONTACT_CONSTANT, cleanBody);
       await req.session.save();
 
       res.statusCode = 200;
--- v1/message.js
+++ v1/message.js
@@ -1,4 +1,7 @@
 import { reqGetHeaders, reqApiHost } from "grandus-lib/utils";
+import { getClientIpAddress } from "grandus-lib/utils/request";
+import { validateHoneypot, checkRateLimit } from "utils/antiSpam";
+import client from "grandus-lib/utils/cache";
 import { get } from "lodash";
 
 export default async (req, res) => {
@@ -6,12 +9,42 @@ export default async (req, res) => {
 
   res.setHeader("Content-Type", "application/json");
 
+  // Anti-spam checks
+  const ip = getClientIpAddress(req);
+
+  // 1. Honeypot validation
+  if (validateHoneypot(body._website)) {
+    return res.status(400).json({
+      error: 'Neplatný request',
+      status: false,
+      data: { messages: [] }
+    });
+  }
+
+  // 2. Rate limiting (max 3 za hodinu)
+  const isRateLimited = await checkRateLimit(ip, client, {
+    keyPrefix: 'contact_form_message',
+    maxRequests: 3,
+    windowSeconds: 3600
+  });
+
+  if (isRateLimited) {
+    return res.status(429).json({
+      error: 'Príliš veľa požiadaviek. Skúste neskôr.',
+      status: false,
+      data: { messages: [] }
+    });
+  }
+
+  // Odstrániť honeypot field pred forward na backend
+  const { _website, ...cleanBody } = body;
+
   let url = `${reqApiHost(req)}/api/v2/contact-forms`;
 
   const response = await fetch(url, {
     headers: reqGetHeaders(req),
     method: "POST",
-    body: body,
+    body: JSON.stringify(cleanBody),
   }).then((result) => result.json());
 
   res.status(get(response, 'statusCode')).json(get(response, 'data'));
